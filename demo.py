c018515d8c11d019e83a03aaa49735da3268d989
Kunal7069 <107999879+Kunal7069@users.noreply.github.com>
2023-12-20 17:01:03 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -47,7 +47,7 @@ def sheet(formatted_date):
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(r"C:\Users\jaink\Desktop\LADLE TRACKER\mysite\mysite\credentials.json",SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json",SCOPES)
             creds = flow.run_local_server(port=0)
         with open("token.json","w") as token:
             token.write(creds.to_json())
